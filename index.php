4ebd6bc30f1aa3f7ff7e4845b42a7fcfd3e2009e
Angeluuh <angelanicolebatoon@gmail.com>
2023-04-14 11:18:34 +0800

    landing page, employee/resident login page, resident signup page

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Serbisyong Aagapay sa Mamayan</title>
+    <!-- Bootstrap CSS link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!-- CSS link -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Google Fonts API link -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="container-fluid p-0">
+        <div class="row vh-100">
+            <div class="col-md-6 d-flex flex-column justify-content-center align-items-center left-container p-0">
+                <div class="col-12 p-5 d-flex flex-column align-items-center justify-content-center">
+                    <p class="fw-semibold m-0 encourage-text">Need help with processing your</p>
+                    <p class="fw-semibold m-0 encourage-text">Barangay Documents and</p>
+                    <p class="fw-semibold encourage-text m-0">Raising Barangay Issues?</p>
+                    <p class="fw-bold text-light d-flex justify-content-center container mt-0 text-break sam-help p-0">SAM can help!</p>
+                    <p class="encourage-text mb-1">We can process your:</p>
+                    <div class="d-flex fw-bold text-light mb-3 p-0">
+                        <div class="col-md-4 d-flex flex-column justify-content-center align-items-center p-4 px-5 mx-1 rounded-3 services-box">
+                            <p class="pt-2 m-0">Barangay</p>
+                            <p class="pb-2">ID</p>
+                        </div>
+                        <div class="col-md-4 d-flex flex-column justify-content-center align-items-center mx-1 rounded-3 services-box">
+                            <p class="pt-2 m-0">Barangay</p>
+                            <p class="pb-2">Clearance</p>
+                        </div>
+                        <div class="col-md-4 d-flex flex-column justify-content-center align-items-center mx-1 rounded-3 services-box">
+                            <p class="pt-2 m-0">Certificate</p>
+                            <p class="m-0">of</p>
+                            <p class="pb-2">Indigency</p>
+                        </div>
+                    </div>
+                    <div class="d-flex fw-bold text-light mb-3 p-0">
+                        <div class="col-md-6 d-flex flex-column justify-content-center align-items-center mx-1 rounded-3 services-box">
+                            <p class="pt-2 m-0">Certificate</p>
+                            <p class="m-0">of</p>
+                            <p class="pb-2">Residency</p>
+                        </div>
+                        <div class="col-md-6 d-flex flex-column justify-content-center align-items-center p-4 px-5 mx-1 rounded-3 services-box">
+                            <p class="pt-2 m-0">Business</p>
+                            <p class="pb-2">Permit</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 d-flex flex-column justify-content-center align-items-center p-0 right-container">
+                <p class="fw-semibold fs-2 chat-sam m-0">Try chatting with</p>
+                <p class="fw-semibold fs-2 chat-sam">SAM now!</p>
+                <div class="row d-flex justify-content-start user-type p-0">
+                    <p class="fs-4 chat-sam mt-4 p-0">I am a...</p>
+                </div>
+                <div class="row fs-4 p-1 rounded-1 m-2 resident-button">
+                    <a href="m_login.html" class="d-flex text-decoration-none text-light justify-content-center px-2">Mamamayan / Resident</a>
+                </div>
+                <div class="row text-light fs-4 p-1 rounded-1 m-2 officials-button">
+                    <a href="a_login.html" class="d-flex text-decoration-none text-light justify-content-center">Barangay Official</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap JS link -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+</body>
+</html>
