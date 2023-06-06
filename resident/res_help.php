d17c74733dec8d4f4454af1774236b25fb820f74
shyberryshrtcke <101873553+shyberryshrtcke@users.noreply.github.com>
2023-06-07 04:34:17 +0800

    create res_help.php
    FAQs frontend and backend.

---
+++ resident/res_help.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <!-- Bootstrap CSS link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!-- AdminLTE CSS link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
+    <!-- CSS link -->
+    <link rel="stylesheet" href="../resident/css/res_help.css">
+    <!-- Font Awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Google Fonts API link -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- Bootstrap JS link -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+   
+</head>
+<body class="sidebar-mini layout-fixed layout-navbar-fixed hold-transition overflow-x-hidden">
+	<div class="wrapper">
+		<?php 
+        if(!session_id()){
+            session_start(); 
+        } 
+        
+        if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+            header("Location: ../index.php");
+            exit;
+        }
+
+            include('navbar.php');
+            include('sidebar.php');
+
+            if($conn->connect_error){
+                die('Failed to connect : '.$conn->connect_error);
+            } else {
+                $stmt = $conn->prepare("CALL SP_GET_FAQ");
+                $stmt->execute();
+                $result = $stmt->get_result();
+            }
+        ?>
+		<div class="content-wrapper" style="background-color: #ffffff!important">
+            <div class="content p-0">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12 p-0 mt-2">
+                            <div class="wrapper">
+                                <h1>Frequently Asked Questions</h1> 
+                                <?php
+                                    $isFirst = true;	            	            		 
+                                    while($row = $result->fetch_assoc()){
+                                ?>
+                                <div class="faq">
+                                    <button class="accordion">
+                                        <?php echo $row['question'] ?>
+                                        <i class="fa-solid fa-chevron-down"></i>
+                                    </button>
+                                    <div class="pannel">
+                                        <p>
+                                            <?php echo $row['answer'] ?>
+                                        </p>
+                                    </div>
+                                </div>
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </div>
+		        </div>
+		    </div>
+		</div>
+	</div>
+
+    <script>
+        var acc = document.getElementsByClassName("accordion");
+        var i;
+
+        for (i = 0; i < acc.length; i++) {
+            acc[i].addEventListener("click", function
+            () {
+                this.classList.toggle("active");
+                this.parentElement.classList.toggle("active");
+
+                var pannel = this.nextElementSibling;
+
+                if (pannel.style.display === "block") {
+                    pannel.style.display = "none";
+                } else {
+                    pannel.style.display = "block";
+                }
+            });
+        }
+    </script>
+	
+	 <!-- AdminLTE JS link -->
+    <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+</body>
+</html>
